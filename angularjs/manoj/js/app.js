335d11a4e7490e888c8eb30e090038b62254ad6e
manojkumar1989 <manoj.k@cubeflick.com>
2014-07-24 18:54:35 +0530

    commited the changes for service

---
+++ angularjs/manoj/js/app.js
@@ -0,0 +1,6 @@
+function Hello($scope, $http) {
+    $http.get('js/record.json').
+        success(function(data) {
+            $scope.greeting = data;
+        });
+}
---
+++ angularjs/manoj/js/services.js
@@ -0,0 +1,10 @@
+
+//creating a service name Employee to fetch data from record.json
+
+var employeeServices = angular.module('employeeServices', ['ngResource']);
+employeeServices.factory('Employee', ['$resource',
+  function($resource){
+    return $resource('js/record.json', {}, {
+      query: {method:'GET',  isArray:true}
+    });
+  }]);
