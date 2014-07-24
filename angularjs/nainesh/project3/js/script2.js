b1e1d0234b8e13c51c25ecdf17b17be1f84a6416
Tom Flick <you@example.com>
2014-07-24 16:48:53 +0530

    Converted to service

--- angularjs/nainesh/project3/js/script2.js
+++ angularjs/nainesh/project3/js/script2.js
@@ -1,10 +1,18 @@
 // Code goes here
 var employApp = angular.module('employApp', []);
 
+employApp.service('empService', ['$http',function($http){
+    this.empListing = function(){
 
-employApp.controller('EmployListCtrl', ['$scope', '$http', function ($scope, $http) {
-    $http.get('emploees.json').success(function (data) {
-        $scope.emploees = data;
-    });
-    $scope.orderProp = 'id';
+        return $http.get('emploees.json').then(function (data) {
+            return data;
+        });
+    }
+
+}]);
+
+employApp.controller('EmployListCtrl', ['$scope', '$http','empService', function ($scope, $http , empService) {
+    empService.empListing().then(function(result){
+        $scope.emploees = result.data;
+    })
 }]);
